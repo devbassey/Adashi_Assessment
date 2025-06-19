0d418213b9ecf0fc2122235025672bb8d6c4fc95
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-06-19 13:41:51 +0100

    Update adashi_query_sheet.sql

--- adashi_query_sheet.sql
+++ adashi_query_sheet.sql
@@ -24,3 +24,27 @@ SELECT
 FROM
     users_customuser
 LIMIT 100;
+
+
+-- SHOW ALL RECORDS IN withdrawals_withdrawal TABLE
+SELECT 
+    *
+FROM
+    withdrawals_withdrawal
+LIMIT 100;
+
+-- RETRIEVE PLANS WITH SPECIFIC DETAILS
+SELECT 
+    name, description, amount
+FROM
+    plans_plan
+WHERE
+    id = '002b48c9f6ec48fdb586bd019a85aa9a';
+    
+-- FINDING PLANS BY TYPE
+SELECT 
+    *
+FROM
+    plans_plan
+WHERE
+    plan_type_id = 1;
