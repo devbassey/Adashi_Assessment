b6d02e2facdf79d4583e1cd1a50fe7f80970ecab
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-06-19 12:55:04 +0100

    create adashi_query_sheet
    This sheet is where all query would be done

---
+++ adashi_query_sheet.sql
@@ -0,0 +1,11 @@
+create database adashi_assessment;
+use adashi_assessment;
+
+-- SHOW ALL TABLE IN THE DATABASE
+SHOW TABLES; -- there are 4 tables in this database
+
+-- SHOW ALL RECORDS IN plans_plan TABLE
+SELECT 
+    *
+FROM
+    plans_plan;
